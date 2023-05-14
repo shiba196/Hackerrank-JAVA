ea21d5659543a85406ceeb2cbfe38b4c5e9787e5
grashibam <86063289+grashibam@users.noreply.github.com>
2023-05-14 10:08:45 +0530

    2nd
    Hackerrank 2nd

---
+++ Java Stdin and Stdout I.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // String myString = scanner.next();
+        int Int1 = scanner.nextInt();
+        int Int2 = scanner.nextInt();
+        int Int3 = scanner.nextInt();
+        scanner.close();
+        
+        // System.out.println("myString is:"+ myString);
+        System.out.println(Int1);
+        System.out.println(Int2);
+        System.out.println(Int3);
+    }
+}
